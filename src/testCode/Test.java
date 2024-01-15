0ffc93a78f9e81640196a9557c1c71c28e56b6c0
Casual1157 <casualwarrior1157@gmail.com>
2024-01-15 10:24:40 +0100

    local commit test end

--- src/testCode/Test.java
+++ src/testCode/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String [] args) {
 		System.out.println("Hello Github");
 		System.out.println("test 2");
+		System.out.println("Local Commit Test End");
 	}
 }
